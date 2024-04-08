612ca8242c69723313b125611f02758ef7e13650
juans <juans@DESKTOP-45N88LE>
2024-04-08 10:37:07 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Juan_Diego_Silva_AtividadClase1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
